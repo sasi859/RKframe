8e58a4e7c73004730a700aa99b434e0d1178a79b
MOHITH <MOHITH@ANUBIS>
2025-04-02 01:13:58 +0530

    created leads module and written testscripts

--- RKFrame/src/main/java/Contant/ToCreateHomepage.java
+++ RKFrame/src/main/java/Contant/ToCreateHomepage.java
@@ -4,6 +4,8 @@ public class ToCreateHomepage {
 
 	public static void main(String[] args) {
 		System.out.println("Homepage created sucessfully");
+		System.out.println("me written testscripts with help of utilities");
+		System.out.println("Me successfully create contact");
 
 	}
 
---
+++ RKFrame/src/main/java/LeadsTests/ToCreateLead.java
@@ -0,0 +1,12 @@
+package LeadsTests;
+
+public class ToCreateLead {
+
+	public static void main(String[] args) {
+		System.out.println("leads created successfully");
+		System.out.println("=========================");
+		System.out.println("Me writing new test scripts");
+
+	}
+
+}
